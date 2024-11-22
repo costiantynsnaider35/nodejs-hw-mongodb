a83a06022acebf750c03fb624a8b67342a1a2298
Ваше ім'я <constantin161089@gmail.com>
2024-11-22 13:49:31 +0200

    conrol,rout,midle

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+import { isHttpError } from 'http-errors';
+
+export function errorHandler(error, req, res, next) {
+  if (isHttpError(error)) {
+    return res
+      .status(error.statusCode)
+      .send({ status: error.statusCode, message: error.message });
+  }
+
+  console.error(error);
+
+  res.status(500).send({ status: 500, message: 'Internal server error' });
+
+  next();
+}
