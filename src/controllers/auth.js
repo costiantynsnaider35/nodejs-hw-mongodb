04c63b1a0e84e908e139851b8662fc7cd3f933bd
Ваше ім'я <constantin161089@gmail.com>
2024-12-09 16:12:00 +0200

    fix register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,21 +1,25 @@
-import { registerUser } from '../services/auth.js';
+import createHttpError from 'http-errors';
+import { createUser } from '../services/auth.js';
 
-export const registerController = async (req, res, next) => {
+export const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    const user = await registerUser({ name, email, password });
+    const user = await createUser({ name, email, password });
 
     res.status(201).json({
-      status: 201,
+      status: 'success',
       message: 'Successfully registered a user!',
       data: {
-        id: user._id,
+        id: user.id,
         name: user.name,
         email: user.email,
       },
     });
   } catch (error) {
+    if (error.code === 11000) {
+      return next(createHttpError(409, 'Email in use'));
+    }
     next(error);
   }
 };
