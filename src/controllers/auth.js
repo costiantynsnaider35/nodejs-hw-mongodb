e909224d669f803d15a1f99c811e1236c52ae998
Ваше ім'я <constantin161089@gmail.com>
2024-12-09 16:46:33 +0200

    step 4,5,6,7

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,12 @@
 import createHttpError from 'http-errors';
-import { createUser } from '../services/auth.js';
+import {
+  createUser,
+  loginUserService,
+  logoutUserSession,
+  refreshSessionTokens,
+} from '../services/auth.js';
+import { createAccessToken, createRefreshToken } from '../services/token.js';
+import Session from '../db/models/session.model.js';
 
 export const registerUser = async (req, res, next) => {
   try {
@@ -20,6 +27,96 @@ export const registerUser = async (req, res, next) => {
     if (error.code === 11000) {
       return next(createHttpError(409, 'Email in use'));
     }
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await loginUserService(email, password);
+
+    const accessToken = createAccessToken(user);
+    const refreshToken = createRefreshToken(user);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Successfully logged in an user!',
+      data: {
+        accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshSession = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return next(createHttpError(401, 'No refresh token found'));
+    }
+
+    const session = await Session.findOne({ refreshToken });
+
+    if (!session) {
+      return next(createHttpError(401, 'Invalid refresh token'));
+    }
+
+    const { accessToken, refreshToken: newRefreshToken } =
+      await refreshSessionTokens(session.userId);
+
+    await session.remove();
+
+    await new Session({
+      userId: session.userId,
+      accessToken,
+      refreshToken: newRefreshToken,
+      accessTokenValidUntil: new Date(Date.now() + 15 * 60 * 1000),
+      refreshTokenValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    }).save();
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Successfully refreshed a session!',
+      data: {
+        accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (req, res, next) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return next(createHttpError(401, 'No refresh token found'));
+    }
+
+    const sessionId = req.cookies.sessionId;
+
+    if (!sessionId) {
+      return next(createHttpError(401, 'No session ID found'));
+    }
+
+    await logoutUserSession(sessionId, refreshToken);
+
+    res.clearCookie('refreshToken');
+    res.clearCookie('sessionId');
+
+    res.status(204).send();
+  } catch (error) {
     next(error);
   }
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/auth.js';
+import {
+  loginUser,
+  logout,
+  refreshSession,
+  registerUser,
+} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerUserSchema } from '../validation/auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
 
 const router = Router();
 
 router.post('/register', validateBody(registerUserSchema), registerUser);
+router.post('/login', validateBody(loginUserSchema), loginUser);
+router.post('/refresh', refreshSession);
+router.post('/logout', logout);
 
 export default router;
